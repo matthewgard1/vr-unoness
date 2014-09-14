4e23c6b7a87efec90b60601ae895d02a04e61c9a
Matthew G <the_piggy1@yahoo.com>
2014-09-13 23:32:59 -0600

    add guard and config variables

--- src/main/scala/vr/unoness/ScalaDisplayTest.scala
+++ src/main/scala/vr/unoness/ScalaDisplayTest.scala
@@ -1,6 +1,6 @@
 package piggydev.unoness.vr
 
-import org.lwjgl.LWJGLException
+import org.lwjgl.{opengl, LWJGLException}
 import org.lwjgl.input.Keyboard
 import org.lwjgl.opengl.Display
 import org.lwjgl.opengl.DisplayMode
@@ -15,17 +15,30 @@ object ScalaDisplayTest {
   def main (args: Array[String]) {
     println("live")
     val freq = 60
-    println("loading modes...")
-    val modes = org.lwjgl.opengl.Display.getAvailableDisplayModes
-    val desired_mode = modes.filter( m => {
-      m.isFullscreenCapable() &&
-      m.getWidth() == 1920 &&
-      m.getFrequency() == freq
-    })
+    val fullscreen = false
+
+    val desired_mode: DisplayMode = {
+      if(fullscreen) {
+        println("loading modes...")
+        val modes = org.lwjgl.opengl.Display.getAvailableDisplayModes
+        modes.find(m => {
+          m.isFullscreenCapable &&
+          m.getWidth == 1920 &&
+          m.getFrequency == freq
+        })
+      } else {
+        None
+      }
+    }.getOrElse {
+      new opengl.DisplayMode(640, 480)
+    }
+
 
     println("Starting")
+    var want_to_end = false
+
     try {
-      Display.setDisplayModeAndFullscreen(desired_mode.head)
+      Display.setDisplayModeAndFullscreen(desired_mode)
       Display.setTitle("Episode 1 - Display Test")
       Display.setFullscreen(true)
       Display.setVSyncEnabled(true)
@@ -35,11 +48,11 @@ object ScalaDisplayTest {
       sys.exit(1)
     }
 
-    while (!Display.isCloseRequested) {
+    while (!Display.isCloseRequested && !want_to_end) {
       Display.update()
       if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)){
         println("DOWN")
-        sys.exit(0)
+        want_to_end = true //sys.exit(0)
       }
       Display.sync(freq)
     }
