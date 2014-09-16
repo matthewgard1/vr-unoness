75e2c76938d7773477209bc24c9910afba591404
Matthew G <the_piggy1@yahoo.com>
2014-09-16 17:32:18 -0600

    test gl11

--- src/main/scala/vr/unoness/ScalaDisplayTest.scala
+++ src/main/scala/vr/unoness/ScalaDisplayTest.scala
@@ -1,9 +1,8 @@
 package piggydev.unoness.vr
 
-import org.lwjgl.{opengl, LWJGLException}
+import org.lwjgl.{Sys, opengl, LWJGLException}
 import org.lwjgl.input.Keyboard
-import org.lwjgl.opengl.Display
-import org.lwjgl.opengl.DisplayMode
+import org.lwjgl.opengl.{GL11, Display, DisplayMode}
 /**
  * If this application shows a blank and responsive window
  * and doesn't throw any errors, you know you have installed lwjgl
@@ -48,12 +47,32 @@ object ScalaDisplayTest {
       sys.exit(1)
     }
 
+    GL11.glMatrixMode(GL11.GL_PROJECTION)
+    GL11.glLoadIdentity()
+    GL11.glOrtho(0, desired_mode.getWidth, 0, desired_mode.getHeight, 1, -1)
+    GL11.glMatrixMode(GL11.GL_MODELVIEW)
+
     while (!Display.isCloseRequested && !want_to_end) {
-      Display.update()
       if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)){
         println("DOWN")
         want_to_end = true //sys.exit(0)
       }
+      // Clear the screen and depth buffer
+      GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT)
+
+      // set the color of the quad (R,G,B,A)
+      GL11.glColor3f(0.5f,0.5f,1.0f)
+
+      // draw quad
+      GL11.glBegin(GL11.GL_QUADS)
+      GL11.glVertex2f(100,100)
+      GL11.glVertex2f(100+200,100)
+      GL11.glVertex2f(100+200,100+200)
+      GL11.glVertex2f(100,100+200)
+      GL11.glEnd()
+
+      Display.update()
+
       Display.sync(freq)
     }
 
@@ -61,6 +80,22 @@ object ScalaDisplayTest {
     sys.exit(0)
   }
 
+  def getTime: Long = Sys.getTime * 1000 / Sys.getTimerResolution
+
+  /**
+   * Calculate the FPS and set it in the title bar
+   */
+  var lastFPS = getTime
+  var fps = 0
+
+  def updateFPS() {
+    if (getTime - lastFPS > 1000) {
+      Display.setTitle("FPS: " + fps)
+      fps = 0
+      lastFPS += 1000
+    }
+    fps += 1
+  }
 
 
 }
